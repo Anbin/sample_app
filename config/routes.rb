21cd43a718285effbff12b2be16069992f40aed6
Anbin <anbinstk@gmail.com>
2010-08-11 23:32:53 -0400

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  get "pages/home"
+  get "pages/about"
+  get "pages/help"
+  get "pages/contact"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
